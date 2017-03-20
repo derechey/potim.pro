59310230d16098f93fad5be7ae33de3edea5f92a
DJpoTIM <djtimiphone@gmail.com>
2017-03-20 17:50:41 +0200

    creating media

--- client/components/header/index.js
+++ client/components/header/index.js
@@ -10,36 +10,55 @@ import {getSetting} from '../../actions'
 
 class Header extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: true
+        };
+    }
+
     componentWillMount() {
         const {getSetting} = this.props;
         getSetting()
     }
 
+    componentDidMount() {
+        window.addEventListener('resize', this.toggleDrawer.bind(this))
+    }
+
+    toggleDrawer(event) {
+        (event.currentTarget.innerWidth <= 900)
+            ? this.setState({open: false})
+            : this.setState({open: true})
+
+    }
+
     renderSideBar() {
         return (
             <div >
-                <Drawer containerStyle={{top: 75,left: 8}} open={true}>
+                <Drawer containerStyle={{top: 75, left: 8}} open={this.state.open}>
                     <MenuItem>Menu Item</MenuItem>
                     <MenuItem>Menu Item 2</MenuItem>
                 </Drawer>
-                <Drawer containerStyle={{top: 75,right: 9}} openSecondary={true} >
-                    <MenuItem>Menu Item</MenuItem>
-                    <MenuItem>Menu Item 2</MenuItem>
+                <Drawer containerStyle={{top: 75, right: 9}} openSecondary={true} open={this.state.open}>
+                    <MenuItem>Seconray Menu Item</MenuItem>
+                    <MenuItem>Seconray Menu Item 2</MenuItem>
                 </Drawer>
             </div>
         );
+
     }
 
-
-
     render() {
+
         return (
             <div>
                 <AppBar
                     title="Title"
                     iconClassNameRight="muidocs-icon-navigation-expand-more"
                 />
-                   {this.renderSideBar()}
+                {this.renderSideBar()}
             </div>
 
         )
