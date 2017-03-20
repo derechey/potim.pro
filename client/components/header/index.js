eb06b29b557c344684f72aff95dedfe188163b83
DJpoTIM <djtimiphone@gmail.com>
2017-03-20 18:17:06 +0200

    creating media

--- client/components/header/index.js
+++ client/components/header/index.js
@@ -3,7 +3,7 @@ import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import {connect} from "react-redux";
-
+import RaisedButton from 'material-ui/RaisedButton';
 import {getSetting} from '../../actions'
 
 @connect(({setting}) => ({...setting}), {getSetting})
@@ -42,8 +42,24 @@ class Header extends Component {
                     <MenuItem>Menu Item 2</MenuItem>
                 </Drawer>
                 <Drawer containerStyle={{top: 75, right: 9}} openSecondary={true} open={this.state.open}>
-                    <MenuItem>Seconray Menu Item</MenuItem>
-                    <MenuItem>Seconray Menu Item 2</MenuItem>
+                    <MenuItem>
+                        <div style={{display:'flex',justifyContent:'center',width:'100%',height:150,marginTop:20}}>
+                            <div  style={{width:150,height:150}}>
+                                <img style={{ width:'100%',borderRadius:'100%'}} src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/17190811_1430105133700696_6986773998192439299_n.jpg?oh=ed1bb03d480afb2b36ab71de8d796bd4&oe=59644C28" alt=""/>
+                            </div>
+                        </div>
+
+                    </MenuItem>
+                    <MenuItem>
+                        <div style={{marginTop:20,display:'flex',justifyContent:'center'}}>
+                            <RaisedButton
+                                href="https://www.facebook.com/profile.php?id=100001036684071&fref=hovercard"
+                                label="Оля Деречей"
+                            />
+                        </div>
+
+
+                       </MenuItem>
                 </Drawer>
             </div>
         );
